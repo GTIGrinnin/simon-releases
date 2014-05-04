a20dbedb878cc68daaa76b0c08630d6dc2d5f539
Arne Franken <arne.franken@coremedia.com>
2014-05-04 23:49:01 +0200

    restructure and rename example config

--- release/1.0.0/config.js
+++ release/1.0.0/config.js
@@ -1,36 +1,37 @@
 configJson = {
-  "title": "A-Team Jobs",
-  "sections": [
+  title: "Main Title",
+  sections: [
     {
-      "hostname": "jenkins-master.com",
-      "sections": [
+      title: "section 1",
+      hostname: "jenkins-master.com",
+      sections: [
         {
-          "title": "test-pipeline-01",
-          "url": "http://jenkins-master/view/pipeline1/",
-          "description": "Branch: <b>Master</b>, Build: <b>Nightly</b>",
-          "monitors": [
+          title: "sub-section 1",
+          url: "http://jenkins-master/view/pipeline1/",
+          description: "Branch: <b>Master</b>, Build: <b>Nightly</b>",
+          monitors: [
             {
-              "name": "CI Job 1",
-              "externalRef": "ci-1"
+              name: "Jenkins Job - CI",
+              externalRef: "ci-1"
             },
             {
-              "name": "Retest System",
-              "externalRef": "test-1"
+              name: "Jenkins Job - Retest System",
+              externalRef: "test-1"
             },
             {
-              "name": "Retest System Smoketests",
-              "externalRef": "smoke-tests-1"
+              name: "Jenkins Job - Retest System Smoketests",
+              externalRef: "smoke-tests-1"
             },
             {
-              "name": "Performance Tests",
-              "externalRef": "performance-tests-1"
+              name: "Jenkins Job - Performance Tests",
+              externalRef: "performance-tests-1"
             },
             {
-              "name": "Sonar",
-              "externalRef": "sonar-1"
+              name: "Jenkins Job - Sonar",
+              externalRef: "sonar-1"
             },
             {
-              name: "Pipeline 1 Sonar",
+              name: "Sonar Analysis",
               type: "sonar",
               hostname: "my-sonar.com",
               externalRef: [
@@ -47,40 +48,126 @@ configJson = {
           ]
         },
         {
-          "title": "test-pipeline-02",
-          "url": "http://jenkins-master/view/pipeline2/",
-          "description": "Branch: <b>Master</b>, Build: <b>Nightly</b>",
-          "monitors": [
+          title: "sub-section 2",
+          url: "http://jenkins-master/view/pipeline2/",
+          description: "Branch: <b>Master</b>, Build: <b>Nightly</b>",
+          monitors: [
             {
-              "name": "CI Job 2",
-              "externalRef": "ci-2"
+              name: "Jenkins Job - CI",
+              externalRef: "ci-2"
             },
             {
-              "name": "Retest System",
-              "externalRef": "test-2"
+              name: "Jenkins Job - Retest System",
+              externalRef: "test-2"
             },
             {
-              "name": "Retest System Smoketests",
-              "externalRef": "smoke-tests-2"
+              name: "Jenkins Job - Retest System Smoketests",
+              externalRef: "smoke-tests-2"
             }
           ]
         },
         {
-          "title": "test-pipeline-03",
-          "url": "http://jenkins-master/view/pipeline3/",
-          "description": "Branch: <b>teamname Feature</b>, Build: <b>Manuell</b>",
-          "monitors": [
+          title: "sub-section 3",
+          url: "http://jenkins-master/view/pipeline3/",
+          description: "Branch: <b>teamname Feature</b>, Build: <b>Manuell</b>",
+          monitors: [
             {
-              "name": "Workspace (Java 7, 64bit)",
-              "externalRef": "ci-3"
+              name: "Jenkins Job - CI",
+              externalRef: "ci-3"
             },
             {
-              "name": "Retest System",
-              "externalRef": "test-3"
+              name: "Jenkins Job - Retest System",
+              externalRef: "test-3"
             },
             {
-              "name": "Retest System Smoketests",
-              "externalRef": "smoke-tests-3"
+              name: "Jenkins Job - Retest System Smoketests",
+              externalRef: "smoke-tests-3"
+            }
+          ]
+        }
+      ]
+    },
+    {
+      title: "section 2",
+      hostname: "jenkins-master.com",
+      sections: [
+        {
+          title: "sub-section 1",
+          url: "http://jenkins-master/view/pipeline1/",
+          description: "Branch: <b>Master</b>, Build: <b>Nightly</b>",
+          monitors: [
+            {
+              name: "Jenkins Job - CI",
+              externalRef: "ci-4"
+            },
+            {
+              name: "Jenkins Job - Retest System",
+              externalRef: "test-4"
+            },
+            {
+              name: "Jenkins Job - Retest System Smoketests",
+              externalRef: "smoke-tests-4"
+            },
+            {
+              name: "Jenkins Job - Performance Tests",
+              externalRef: "performance-tests-4"
+            },
+            {
+              name: "Jenkins Job - Sonar",
+              externalRef: "sonar-4"
+            },
+            {
+              name: "Sonar Analysis",
+              type: "sonar",
+              hostname: "my-sonar.com",
+              externalRef: [
+                {
+                  name: "All modules",
+                  id: "my.company:project:profile"
+                },
+                {
+                  name: "module",
+                  id: "my.company:module:profile"
+                }
+              ]
+            }
+          ]
+        },
+        {
+          title: "sub-section 2",
+          url: "http://jenkins-master/view/pipeline2/",
+          description: "Branch: <b>Master</b>, Build: <b>Nightly</b>",
+          monitors: [
+            {
+              name: "Jenkins Job - CI",
+              externalRef: "ci-5"
+            },
+            {
+              name: "Jenkins Job - Retest System",
+              externalRef: "test-5"
+            },
+            {
+              name: "Jenkins Job - Retest System Smoketests",
+              externalRef: "smoke-tests-5"
+            }
+          ]
+        },
+        {
+          title: "sub-section 3",
+          url: "http://jenkins-master/view/pipeline3/",
+          description: "Branch: <b>teamname Feature</b>, Build: <b>Manuell</b>",
+          monitors: [
+            {
+              name: "Jenkins Job - CI",
+              externalRef: "ci-6"
+            },
+            {
+              name: "Jenkins Job - Retest System",
+              externalRef: "test-6"
+            },
+            {
+              name: "Jenkins Job - Retest System Smoketests",
+              externalRef: "smoke-tests-6"
             }
           ]
         }
